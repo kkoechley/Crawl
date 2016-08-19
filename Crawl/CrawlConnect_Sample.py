ff22ed38ef6916c41506aab4a66b96b32329bea5
Rutherford Le <rutherfordle@Rutherfords-MacBook-Pro.local>
2016-08-19 14:50:49 -0700

    Connect update

--- Crawl/CrawlConnect_Sample.py
+++ Crawl/CrawlConnect_Sample.py
@@ -9,6 +9,6 @@ driver.get('https://pulse.conviva.com/login?next=/reports/15/')
 username = driver.find_element_by_name("username")
 password = driver.find_element_by_name("password")
 
-username.send_keys("username")
-password.send_keys("password")
+username.send_keys("Change this to your username")
+password.send_keys("Change this to your password")
 
